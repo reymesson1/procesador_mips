9bf62375b9142c84c5453182841a0bf18bc71d1e
reymesson1 <reymesson1>
2020-08-01 21:27:40 -0400

    third commit

---
+++ project_2/project_2.srcs/sources_1/new/Procesador.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.08.2020 19:18:17
+// Design Name: 
+// Module Name: Procesador
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Procesador(
+    input [15:0] a,
+    input [15:0] b
+    );
+endmodule
