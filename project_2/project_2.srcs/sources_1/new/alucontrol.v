a40fd4b873be368ccbe29e0384bc09dcf2c7fb6d
albertdeleon3 <69093746+albertdeleon3@users.noreply.github.com>
2020-08-01 21:48:06 -0400

    alucontrol,control,datapath

---
+++ project_2/project_2.srcs/sources_1/new/alucontrol.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/01/2020 09:43:12 PM
+// Design Name: 
+// Module Name: alucontrol
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alucontrol(
+
+    );
+endmodule
